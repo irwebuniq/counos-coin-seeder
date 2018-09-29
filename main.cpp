b94ea47db671a20652607389ecf8d7f132a487cf
Iman <eng.mousavi86@gmail.com>
2018-09-29 19:50:33 +0330

    Update parameters

--- main.cpp
+++ main.cpp
@@ -397,17 +397,13 @@ extern "C" void* ThreadStats(void*) {
   return nullptr;
 }
 
-static const string mainnet_seeds[] = {"dnsseed.bluematt.me", "bitseed.xf2.org", "dnsseed.bitcoin.dashjr.org", "seed.bitcoin.sipa.be", ""};
-static const string testnet_seeds[] = {"testnet-seed.alexykot.me",
-                                       "testnet-seed.bitcoin.petertodd.org",
-                                       "testnet-seed.bluematt.me",
-                                       "testnet-seed.bitcoin.schildbach.de",
-                                       ""};
+static const string mainnet_seeds[] = {"dnsseed.counos.net", ""};
+static const string testnet_seeds[] = {""};
 static const string *seeds = mainnet_seeds;
 
 extern "C" void* ThreadSeeder(void*) {
   if (!fTestNet){
-    db.Add(CService("srvcoin1.counos.com", 11963), true);
+    db.Add(CService("kjy2eqzk4zwi5zd3.onion", 11963), true);
   }
   do {
     for (int i=0; seeds[i] != ""; i++) {
@@ -459,10 +455,10 @@ int main(int argc, char **argv) {
   bool fDNS = true;
   if (opts.fUseTestNet) {
       printf("Using testnet.\n");
-      pchMessageStart[0] = 0x0b;
-      pchMessageStart[1] = 0x11;
-      pchMessageStart[2] = 0x09;
-      pchMessageStart[3] = 0x07;
+      pchMessageStart[0] = 0xfd;
+      pchMessageStart[1] = 0xc8;
+      pchMessageStart[2] = 0xc8;
+      pchMessageStart[3] = 0xf1;
       seeds = testnet_seeds;
       fTestNet = true;
   }
